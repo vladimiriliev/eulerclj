2494c2742ec721195a5a5ed5fc86adfef9f34099
Vladimir Iliev <iliev.vlad@gmail.com>
2015-08-25 00:01:48 +0300

    Add inline documentation
    Add inline documentation.

--- src/eulerclj/multiplesOf3And5.clj
+++ src/eulerclj/multiplesOf3And5.clj
@@ -1,4 +1,6 @@
 (ns eulerclj.multiplesOf3And5)
 
-(defn solution[]
+(defn solution
+  "Loop over the range 0 - 1000 and sum multiples of 3 and 5."
+  []
 (reduce #(if (or (= (mod %2 3) 0) (= (mod %2 5) 0)) (+ %1 %2) %1) (range 0 1000)))
